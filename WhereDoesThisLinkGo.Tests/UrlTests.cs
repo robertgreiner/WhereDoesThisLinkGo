f5f45f764f39f984c0c212970fcb252ad093bc77
Robert Greiner <robert@robertgreiner.com>
2014-04-04 12:30:51 -0500

    update project

---
+++ WhereDoesThisLinkGo.Tests/UrlTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using WhereDoesThisLinkGo.Services;
+
+namespace WhereDoesThisLinkGo.Tests
+{
+    [TestFixture]
+    public class UrlTests
+    {
+        private IUrlService service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            service = new UrlService();
+        }
+
+        [Test]
+        public void ShouldExpandShortenedUrl()
+        {
+            var uri = service.Expand("http://goo.gl/s4i0").AbsoluteUri;
+            Assert.AreEqual("http://robertgreiner.com/", uri);
+        }
+
+        [Test]
+        public void ShouldReturnSameUrlWhenNotShortened()
+        {
+            var uri = service.Expand("http://robertgreiner.com").AbsoluteUri;
+            Assert.AreEqual("http://robertgreiner.com/", uri);
+        }
+
+        [Test]
+        public void InvalidUrl()
+        {
+            var uri = service.Expand("http://qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq.com/1234");
+            Assert.AreEqual(null, uri);
+        }
+
+        [Test]
+        public void ShouldProcessShortenedUrlWithoutHttpPrefix()
+        {
+            var uri = service.Expand("goo.gl/s4i0").AbsoluteUri;
+            Assert.AreEqual("http://robertgreiner.com/", uri);
+        }
+    }
+}
---
+++ WhereDoesThisLinkGo/Models/UrlViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhereDoesThisLinkGo.Models
+{
+    public class UrlViewModel
+    {
+        public string ShortenedUri { get; set; }
+        public Uri ExpandedUri { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
